4c6a6433bba19bf9fed75de99c9962921d3be78a
aeoa <64489515+aeoa@users.noreply.github.com>
2025-10-26 09:49:55 +0100

    restore M0

--- firmware/hackrf_usb/sgpio_m0.s
+++ firmware/hackrf_usb/sgpio_m0.s
@@ -247,7 +247,6 @@ The rest of this file is organised as follows:
 .equ SLICE5,                               16
 .equ SLICE6,                               32
 .equ SLICE7,                               0
-.equ SLICE_HOST,                           48
 
 /* Allocations of single-use registers */
 
@@ -712,10 +711,9 @@ rx_loop:
 	ldr r0, [sgpio_data, #SLICE4]                   // r0 = SGPIO_REG_SS[SLICE4]            // 10
 	ldr r1, [sgpio_data, #SLICE5]                   // r1 = SGPIO_REG_SS[SLICE5]            // 10
 	ldr r2, [sgpio_data, #SLICE6]                   // r2 = SGPIO_REG_SS[SLICE6]            // 10
-	ldr r3, [sgpio_data, #SLICE_HOST]               // r3 = SGPIO_REG_SS[HOST]             // 10
-	lsl r3, r3, #16                                 // keep lower 16 bits only              // 1
-	lsr r3, r3, #16                                 // restore masked value                 // 1
+	ldr r3, [sgpio_data, #SLICE7]                   // r3 = SGPIO_REG_SS[SLICE7]            // 10
 	stm buf_ptr!, {r0-r3}                           // buf_ptr[0:16] = r0-r3; buf_ptr += 16 // 5
+
 	// Update counts.
 	update_counts                                   // update_counts()                      // 4
 
